26b2746ce97f6ae5c7deb7e9b4b9c4efd4c2f56d
guido fortunato <guido_fortunato@hotmail.com.ar>
2022-06-09 13:30:52 -0300

    cambios en los nombres

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,48 @@
-import React, { useEffect } from 'react';
-import Section from './components/main/Section';
-import SectionHistoria from './components/main/SectionHistoria';
-import Imagenes from './components/main/Imagenes';
+import React, { useEffect } from "react";
+import Section from "./components/main/Section";
+import SectionHistoria from "./components/main/SectionHistoria";
+import Imagenes from "./components/main/Imagenes";
 // import Contacto from './components/main/Contacto';
-import Footer from './components/footer/Footer';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-  
-  } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/footer/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 //import BotonFlotante from './components/BotonFlotante';
-import Eventos from './components/main/Eventos';
+import Eventos from "./components/main/Eventos";
 // import DetalleEvento from './components/EventDetail/DetalleEvento';
-import VariablesProvider from './context/VariablesProvider';
-import TagManager from 'react-gtm-module';
+import VariablesProvider from "./context/VariablesProvider";
+import TagManager from "react-gtm-module";
 //import Error404 from './components/auth/Error404';
 
-
 const TagManagerArgs = {
-  gtmId: 'GTM-N2N9W5J'
-}
+  gtmId: "GTM-N2N9W5J",
+};
 
-TagManager.initialize(TagManagerArgs)
-
-
+TagManager.initialize(TagManagerArgs);
 
 function App() {
-  
   useEffect(() => {
-    
-    TagManager.initialize(TagManagerArgs)
-    
-  }, [])
-
-
-
-
+    TagManager.initialize(TagManagerArgs);
+  }, []);
 
   return (
     <>
-    <VariablesProvider>
-
-      <Router basename={'/gira/ciro'}> 
-      
-
-        <Switch>
-          <Route path='/' exact>
-            <Navbar/>
-            <Section />         
-            <Eventos />
-            <SectionHistoria />
-            <Imagenes />
-            {/* <Contacto /> */}
-            {/* <BotonFlotante/> */}
-            <Footer />           
-          </Route> 
-
-          {/* <Route path='/:id' exact >
-            <DetalleEvento />            
-          </Route>        */}
-
-
-          {/* <Route path='*' component={Error404} /> */}
-        
-        </Switch>
-      </Router>
-    </VariablesProvider>
+      <VariablesProvider>
+        <Router basename={"/gira/lpda"}>
+          <Switch>
+            <Route path="/" exact>
+              <Navbar />
+              <Section />
+              <Eventos />
+              <SectionHistoria />
+              <Imagenes />
+              {/* <Contacto /> */}
+              {/* <BotonFlotante/> */}
+              <Footer />
+            </Route>
+          </Switch>
+        </Router>
+      </VariablesProvider>
     </>
-
-  
-  
   );
 }
 
